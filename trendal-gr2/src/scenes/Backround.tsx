7df31b17cf0362f614ffdc6268428ae7a8c0c5a7
JD-Almeida <jacquesalmeida2003@gmail.com>
2024-03-26 08:18:20 +0000

    Added Landing Page Text

--- trendal-gr2/src/scenes/Backround.tsx
+++ trendal-gr2/src/scenes/Backround.tsx
@@ -1,5 +1,6 @@
 import backroundPng from "/src/assets/image 2.png";
 import bBlur from "/src/assets/Blur.png";
+import Search from "./Search";
 import "/src/index.css";
 
 const blurB = () => {
@@ -7,6 +8,17 @@ const blurB = () => {
     <div>
       <img alt="backround" src={backroundPng} className="backroundImg"></img>
       <img alt="backroundLayer" src={bBlur} className="backroungFade"></img>
+      <div className="landing-text-container">
+        <h1 className="title">Welcome to Beige</h1>
+        <h3 className="flavour-text">
+          Introducing the all-new Web Based AI supported potted artefact sorter
+          created in collaboration with La Trobe University.
+        </h3>
+        <h3 className="flavour-text">
+          If you’re new or need a reminder, explore the “Filter Guide” below.
+        </h3>
+        <Search></Search>
+      </div>
     </div>
   );
 };
