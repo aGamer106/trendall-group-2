78ead78b42541ae64639ccdc9c1d2ce6ec0d72f6
aGamer106 <agamer106@yahoo.com>
2024-03-26 12:46:55 +0000

    modified app.py to extract the text from the pdf file

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+import re
+
+import pdfminer.high_level
+from pdfminer.high_level import extract_pages, extract_text
+from flask import Flask, redirect, url_for, render_template, request, flash
 
 app = Flask(__name__)
 
+PDF_PATH = r"D:\TrendalDocumentation.pdf"
+
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def main():  # put application's code here
+    text = pdfminer.high_level.extract_text(PDF_PATH, maxpages=10)
+    return render_template("index.html", text_data=text[:1000])
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
